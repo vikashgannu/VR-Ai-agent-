95fb0805a3909a3d5a07ac2db70369afd3b43ce6
vikashgannu <vikashgurjargnnu@gmail.com>
2026-07-07 22:38:24 +0530

    Create uploader.py

---
+++ youtube/uploader.py
@@ -0,0 +1,7 @@
+class YouTubeUploader:
+
+    def upload(self, title):
+        print(f"Uploading video: {title}")
+
+    def schedule(self, title, time):
+        print(f"Scheduling '{title}' at {time}")
