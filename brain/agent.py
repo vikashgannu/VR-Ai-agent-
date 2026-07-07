e2f5ef01a141890991627edcb8ef56aff9c81fa9
vikashgannu <vikashgurjargnnu@gmail.com>
2026-07-07 23:00:53 +0530

    Update agent.py

--- brain/agent.py
+++ brain/agent.py
@@ -1,15 +1,21 @@
 from brain.commands import CommandProcessor
 from memory.memory import Memory
+from tasks.task_manager import TaskManager
 
 class VRAIAgent:
+
     def __init__(self):
+
         self.processor = CommandProcessor()
         self.memory = Memory()
+        self.task = TaskManager()
 
     def run(self, command):
+
         self.memory.save(command)
 
         result = self.processor.process(command)
 
-        print("Task :", result["task"])
+        self.task.execute(result["task"])
+
         print("History :", self.memory.show())
