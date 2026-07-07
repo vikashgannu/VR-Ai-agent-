2c178a8b9e9053c312e55b68d0b752a4f33cf50d
vikashgannu <vikashgurjargnnu@gmail.com>
2026-07-07 22:39:07 +0530

    Create writer.py

---
+++ script/writer.py
@@ -0,0 +1,15 @@
+class ScriptWriter:
+
+    def create(self, topic):
+        return f"""
+Title: {topic}
+
+Hook:
+आज की कहानी आपकी ज़िंदगी बदल सकती है।
+
+Story:
+...
+
+Ending:
+अगर वीडियो पसंद आया तो चैनल को Subscribe करें।
+"""
